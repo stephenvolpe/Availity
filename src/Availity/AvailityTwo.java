62c7770596a60374cbbed54297ad297bc5026fcc
stephenvolpe <stevevolpe@live.com>
2021-08-07 00:34:15 -0400

    read from a file and write to a file.  Honestly should check if the file exists, if it does, then read THAT file in and update that file to maintain the ordering this method has made.  I think it's outside of the scope but crossed my mind.

--- src/Availity/AvailityTwo.java
+++ src/Availity/AvailityTwo.java
@@ -1,28 +1,29 @@
 package Availity;
 
 import Enrollees.Enrollees;
-import javafx.util.Pair;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Collections;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class AvailityTwo {
-    Map<Pair<String, String>, Enrollees> records = new TreeMap<>();
+    public static final String inputPath = "C:\\Users\\sjv\\Availity\\inputs\\";
+    public static final String oututPath = "C:\\Users\\sjv\\Availity\\outputs\\";
+
     public static void main (String [] args) {
 
         String input = "TomUno,Tom,Uno,1,CompanyA\n";
         input += "TimDos,Tim,Dos,1,CompanyB\n";
         input += "JimTres,Tim,Tres,1,CompanyC\n";
         input += "TomUno,Tom,Uno,2,CompanyA\n";
-        readAfileAndSortByLastAndFirstNameAscendingAndRemoveDuplicateUserIDsForTheSameInsuranceCompany(input);
+        //readAfileAndSortByLastAndFirstNameAscendingAndRemoveDuplicateUserIDsForTheSameInsuranceCompany(input);
+        readAfileAndSortByLastAndFirstNameAscendingAndRemoveDuplicateUserIDsForTheSameInsuranceCompany(new File (inputPath + "input.txt"));
     }
 
     public static void readAfileAndSortByLastAndFirstNameAscendingAndRemoveDuplicateUserIDsForTheSameInsuranceCompany (String input) {
@@ -49,9 +50,6 @@ public class AvailityTwo {
 
                 //Lastly, if there are duplicate User Ids for the same Insurance Company, then only the record with the highest version should be included. (Doesn't talk about ties?)
                 Enrollees enrollee = new Enrollees(tokens[0], tokens[1], tokens[2], version, tokens[4]);  //read the content of the file and separate enrollees by insurance company in its own file.
-
-
-
                 //Additionally, sort the contents of each file by last and first name (ascending) //PriorityQueue with the key of Company
                 if (companyToEnrollees.containsKey(enrollee.getInsuranceCompany())) {
                     PriorityQueue<Enrollees> pQueue = companyToEnrollees.get(enrollee.getInsuranceCompany());
@@ -79,7 +77,7 @@ public class AvailityTwo {
                     pQueue.add(enrollee);
                 }
             }
-            scanner.close();
+            scanner.close(); //Honestly can't remember if this should be in a finally as the try catch is very specific in the code (I didnt want this entire code block in a try/catch
             for (Map.Entry<String, PriorityQueue<Enrollees>> kvp : companyToEnrollees.entrySet()) {
                 for (Enrollees e : kvp.getValue()) {
                     System.out.println(e.formatForFile());
@@ -89,174 +87,92 @@ public class AvailityTwo {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public static File readAfileAndSortByLastAndFirstNameAscendingAndRemoveDuplicateUserIDsForTheSameInsuranceCompany (File file) {
-        Map<Pair<String, String>, Enrollees> CompanyAndUserIdTOEnrollees = new HashMap<>();
-        Map<String, PriorityQueue<Enrollees>> companyToEnrollees = new HashMap<>();
-
-        int lengthOfEnrollee = Enrollees.class.getFields().length;
+    public static void readAfileAndSortByLastAndFirstNameAscendingAndRemoveDuplicateUserIDsForTheSameInsuranceCompany (File file) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        Map<String, PriorityQueue<Enrollees>> companyToEnrollees = new TreeMap<>((Comparator<String>) (s1, s2) -> s1.compareTo(s2));
+        Scanner scanner;
         try {
-            Scanner scanner = new Scanner(file);
-            while(scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                //There are libraries to read this with java, i'd prefer to use Guava Splitter as I think it's cleaner but I am trying to stay with STD lib as I think that's what the question is asking for...
-                String [] tokens = line.trim().split(",");
-                if (tokens.length != lengthOfEnrollee) {
-                    System.out.println("Length of fields mismatch - expecting : " + lengthOfEnrollee + " while getting: " + tokens.length);
-                    continue;
-                }
-                int version;
-                try {
-                    version = Integer.parseInt(tokens[3]);
-                } catch (NumberFormatException e) {
-                    System.out.println("Found: " + tokens[3] + " when expecting an integer.");
-                    continue;
-                }
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Can\'t find file: " + file.getAbsolutePath() + " to read.");
+            return;
+        }
+        int lengthOfEnrollee = 5;
+        while(scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            //There are libraries to read this with java, i'd prefer to use Guava Splitter as I think it's cleaner but I am trying to stay with STD lib as I think that's what the question is asking for...
+            String [] tokens = line.trim().split(",");
+            if (tokens.length != lengthOfEnrollee) {
+                System.out.println("Length of fields mismatch - expecting : " + lengthOfEnrollee + " while getting: " + tokens.length);
+                continue;
+            }
+            int version;
+            try {
+                version = Integer.parseInt(tokens[3]);
+            } catch (NumberFormatException e) {
+                System.out.println("Found: " + tokens[3] + " when expecting an integer.  Skipping this line.");
+                continue;
+            }
 
 
-                //Lastly, if there are duplicate User Ids for the same Insurance Company, then only the record with the highest version should be included. (Doesn't talk about ties?)
-                Enrollees enrollee = new Enrollees(tokens[0], tokens[1], tokens[2], version,tokens[4]);  //read the content of the file and separate enrollees by insurance company in its own file.
+            //Lastly, if there are duplicate User Ids for the same Insurance Company, then only the record with the highest version should be included. (Doesn't talk about ties?)
+            Enrollees enrollee = new Enrollees(tokens[0], tokens[1], tokens[2], version, tokens[4]);  //read the content of the file and separate enrollees by insurance company in its own file.
 
 
 
-                //Additionally, sort the contents of each file by last and first name (ascending) //PriorityQueue with the key of Company
-                if (companyToEnrollees.containsKey(enrollee.getInsuranceCompany())) {
-                    PriorityQueue<Enrollees> pQueue = companyToEnrollees.get(enrollee.getInsuranceCompany());
-                    if (pQueue.contains(enrollee.getUserID())) {
-                        //This is not what a priorityQueue is designed for (indexing)
-                        for (Enrollees enr : pQueue) {
-                            if (!enr.equals(enrollee)) {
-                                continue;
-                            }
-
-                            //if the incoming enrollee version is higher, then it replaces the other one.
-                            if (enrollee.getVersion() > enr.getVersion()) {
-                                pQueue.remove(enr);
-                                pQueue.add(enrollee);
-                                break;
-                            }
+            //Additionally, sort the contents of each file by last and first name (ascending) //PriorityQueue with the key of Company
+            if (companyToEnrollees.containsKey(enrollee.getInsuranceCompany())) {
+                PriorityQueue<Enrollees> pQueue = companyToEnrollees.get(enrollee.getInsuranceCompany());
+                if (pQueue.contains(enrollee)) {
+                    //This is not what a priorityQueue is designed for (indexing)
+                    for (Enrollees enr : pQueue) {
+                        if (!enr.equals(enrollee)) {
+                            continue;
                         }
 
-                    } else {
-                        pQueue.add(enrollee);
+                        //if the incoming enrollee version is higher, then it replaces the other one.
+                        if (enrollee.getVersion() > enr.getVersion()) {
+                            pQueue.remove(enr);
+                            pQueue.add(enrollee);
+                            break;
+                        }
                     }
-                    PriorityQueue<Enrollees> thisPQ = companyToEnrollees.get(enrollee.getInsuranceCompany());
-                    thisPQ.add(enrollee);
-                    companyToEnrollees.put(enrollee.getInsuranceCompany(), thisPQ);
-                } else { // Need to stand up the pQueue
-                    PriorityQueue<Enrollees> pQueue = new PriorityQueue<Enrollees>( new Comparator<Enrollees>() {
-                        @Override
-                        public int compare(Enrollees e1, Enrollees e2) {
-                            return Enrollees.compareLastAndFirstName(e1,e2);
-                        }
-                    });
-                    companyToEnrollees.put(enrollee.getInsuranceCompany(), pQueue);
+
+                } else {
+                    pQueue.add(enrollee);
                 }
+            } else { // Need to stand up the pQueue
+                PriorityQueue<Enrollees> pQueue = new PriorityQueue<Enrollees>((Comparator<Enrollees>) (e1, e2) -> Enrollees.compareLastAndFirstName(e1,e2));
+                companyToEnrollees.put(enrollee.getInsuranceCompany(), pQueue);
+                pQueue.add(enrollee);
             }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
-        return new File("C:\\Ok.txt");
-    }
+        scanner.close(); //Honestly can't remember if this should be in a finally as the try catch is very specific in the code (I didnt want this entire code block in a try/catch
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //I am sure there is some better way to do this with optional or something but I'm not sure and overloading this seems to be "acceptable" to me.
-    public static Map<Pair<String, String>, Enrollees> processCSV(File file) {
-        return processCSV(file, new HashMap<>());
-    }
-
-    public static Map<Pair<String, String>, Enrollees> processCSV(File file, HashMap<Pair<String, String>, Enrollees> inputRecords) {
-        List<String> lines;
-        try {
-            lines = readFileIntoList(file);
-        } catch (FileNotCSVException e) {
-            e.printStackTrace();
-            return inputRecords;
-        }
-        return inputRecords;
-    }
-
-
-
-
-
-    public static List<String> readFileIntoList(File file) throws FileNotCSVException {
-        List<String> lines = Collections.emptyList();
-        if (!file.getPath().endsWith(".csv")) {
-            throw new FileNotCSVException();
-        }
-        try {
-            Scanner scanner = new Scanner(file);
-            while(scanner.hasNextLine()) {
-                lines.add(scanner.nextLine());
+        for (Map.Entry<String, PriorityQueue<Enrollees>> kvp : companyToEnrollees.entrySet()) {
+            for (Enrollees e : kvp.getValue()) {
+                System.out.println(e.formatForFile());
             }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
-        return lines;
-    }
 
 
-    private static class FileNotCSVException extends Exception {
-        //this is awful but i don't think this is what we care about...
-        public static void main(String [] args) {
-            System.out.println("This isn't a CSV file.");
+        for (Map.Entry<String, PriorityQueue<Enrollees>> kvp : companyToEnrollees.entrySet()) {
+            String newPath = oututPath + kvp.getKey() + ".txt";
+            FileWriter fileWriter;
+            try {
+                fileWriter = new FileWriter(newPath);
+                for (Enrollees e : kvp.getValue()) {
+                    fileWriter.write(e.formatForFile());
+                }
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Error trying to write to file: " + newPath);
+            }
         }
+
     }
 }
